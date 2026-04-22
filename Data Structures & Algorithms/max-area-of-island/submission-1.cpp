5ee485dad53b79fe0ab150c7413bcca9240ccf56
Sagnik Panda <98590203+sagnik-05@users.noreply.github.com>
2026-04-22 19:04:35 +0530

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int n, m;
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=n || j>=m || !grid[i][j]) return 0;
+        grid[i][j] = 0;
+        return 1 + dfs(grid, i+1, j) +
+        dfs(grid, i-1, j)+
+        dfs(grid, i, j+1)+
+        dfs(grid, i, j-1);
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+        int maxArea = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j]){
+                    int area = dfs(grid, i, j);
+                    maxArea = max(area, maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+};
