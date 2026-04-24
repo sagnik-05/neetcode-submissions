73f4cfdbf9bb6da8adc8eaac0fda0d7275a32fa6
Sagnik Panda <98590203+sagnik-05@users.noreply.github.com>
2026-04-24 12:52:04 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adjList, vector<int>& vis, int parent){
+        vis[node] = 1;
+        for(auto adjNode : adjList[node]){
+            if(!vis[adjNode]){
+                if(dfs(adjNode, adjList, vis, node)) return true;
+            }
+            else if(vis[adjNode] && adjNode!=parent) return true;
+        }
+        vis[node] = 0;
+        return false;
+
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> vis(n, 0);
+        vector<vector<int>> adjList(n);
+        for(auto e : edges){
+            int u = e[0]-1, v = e[1]-1;
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+
+            if(dfs(u, adjList, vis, -1)) return {e[0], e[1]};
+
+        }
+        return {-1,-1};
+    }
+};
